9ccdc388ea1e45abbd3002afd44e309599e2c852
Adam Shen <adamshen1997@gmail.com>
2017-10-07 22:52:23 -0700

    upload

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var requests  = require('requests')
 //var upload = multer({ dest: 'uploads/' })
 
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
+app.get('/', function (req, res) {
+   res.send('Hello World!')
+ })
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
