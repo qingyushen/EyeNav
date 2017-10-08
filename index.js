f40d51ffca44c1a55ab3c058e84af34dfcb64b16
Adam Shen <adamshen1997@gmail.com>
2017-10-07 21:02:29 -0700

    DANGER
    Index.js ALL changed.
    DANGER DANGER

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
+const Vision = require('@google-cloud/vision');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('stufffff')
+})
+const fs = require ('fs')
+
+app.post('/image', upload.single('file'), function (req, res, next) {
+  console.log('got image', req.file)
+  // Instantiates a client
+  const vision = Vision();
+  // The path to the local image file, e.g. "/path/to/image.png"
+   var fileName = req.file.path+'.jpg';
+   var landmarks = {}
+  // Performs landmark detection on the local file
+  vision.landmarkDetection({ source: {filename: fileName} })
+    .then((results) => {
+      landmarks = results[0].landmarkAnnotations;
+      console.log('Landmarks:');
+      landmarks.forEach((landmark) => console.log(landmark));
+    })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+  //console.log(Object.keys(req.body))
+  res.send(landmarks)
 })
 
+
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Server up and running')
 })
+
+//app.post('/upload', upload.single('photo'),())
